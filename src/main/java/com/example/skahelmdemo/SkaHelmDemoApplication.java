fafc1d5cb82c0d828139a0ba404e388ac9bba922
sihandeng <sihan.deng@thoughtworks.com>
2022-12-27 17:03:17 +0800

    init

---
+++ src/main/java/com/example/skahelmdemo/SkaHelmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.skahelmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkaHelmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkaHelmDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skahelmdemo/SkaHelmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skahelmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaHelmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
